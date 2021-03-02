bf2ce46c7fa82432497cedf4ae70ac2a335bfaf5
Павел <tsyganov.p@gmail.com>
2021-03-02 21:35:30 +0300

    Сделал главную

--- js/script-index.js
+++ js/script-index.js
@@ -25,3 +25,58 @@ closeModalBtn.addEventListener("click",function () {
   feedbackModal.classList.remove("feedback-popup--show");
 });
 
+var page = document.querySelector(".main-body");
+var sliderCheckOne = document.querySelector(".slider-icons__icon--1");
+var sliderCheckTwo = document.querySelector(".slider-icons__icon--2");
+var sliderCheckThree = document.querySelector(".slider-icons__icon--3");
+var sliderOne = document.querySelector(".slider__img-container--one");
+var sliderTwo = document.querySelector(".slider__img-container--two");
+var sliderThree = document.querySelector(".slider__img-container--three");
+sliderCheckOne.addEventListener("click", function() {
+  sliderOne.classList.add("slider__img-container--active");
+  sliderTwo.classList.remove("slider__img-container--active");
+  sliderThree.classList.remove("slider__img-container--active");
+
+  sliderCheckOne.classList.add("slider-icons__icon--active");
+  sliderCheckTwo.classList.remove("slider-icons__icon--active");
+  sliderCheckThree.classList.remove("slider-icons__icon--active");
+
+  page.classList.add("slider-1--active");
+  page.classList.remove("slider-2--active");
+  page.classList.remove("slider-3--active");
+});
+
+  sliderCheckTwo.addEventListener("click", function() {
+    sliderOne.classList.remove("slider__img-container--active");
+    sliderTwo.classList.add("slider__img-container--active");
+    sliderThree.classList.remove("slider__img-container--active");
+
+    page.classList.add("slider-1--active");
+    page.classList.remove("slider-2--active");
+    page.classList.remove("slider-3--active");
+    
+
+    sliderCheckOne.classList.remove("slider-icons__icon--active");
+    sliderCheckTwo.classList.add("slider-icons__icon--active");
+    sliderCheckThree.classList.remove("slider-icons__icon--active");
+
+    page.classList.remove("slider-1--active");
+    page.classList.add("slider-2--active");
+    page.classList.remove("slider-3--active");
+});
+
+sliderCheckThree.addEventListener("click", function() {
+  sliderOne.classList.remove("slider__img-container--active");
+  sliderTwo.classList.remove("slider__img-container--active");
+  sliderThree.classList.add("slider__img-container--active");
+
+  sliderCheckOne.classList.remove("slider-icons__icon--active");
+  sliderCheckTwo.classList.remove("slider-icons__icon--active");
+  sliderCheckThree.classList.add("slider-icons__icon--active");
+
+  page.classList.remove("slider-1--active");
+  page.classList.remove("slider-2--active");
+  page.classList.add("slider-3--active");
+});
+
+
