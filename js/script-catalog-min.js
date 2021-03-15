6c5df77078ce616f343463dc97da3e08c3d7b0b7
Павел <tsyganov.p@gmail.com>
2021-03-15 09:59:37 +0300

    Минимизация файлов

---
+++ js/script-catalog-min.js
@@ -0,0 +1,1 @@
+var modalCart=document.querySelector(".cart-preview"),modalCartLink=document.querySelector(".user-icons__cart");modalCartLink.addEventListener("click",(function(){modalCart.classList.toggle("cart-preview--show")}));
